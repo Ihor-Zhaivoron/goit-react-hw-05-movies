ad282ec018c35d7e7d43b4ea582cbef1c19f6b36
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-05-07 20:36:29 +0300

    plan

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from 'path/to/pages/Home';
-import Movies from 'path/to/pages/Movies';
-import NotFound from 'path/to/pages/NotFound';
+import Home from 'path/Home/Home';
+import { Layout } from './Layout/Layout';
+// import NotFound from 'path/to/pages/NotFound';
+import { lazy, Suspense } from 'react';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
+const Movies = lazy(() => import('../page/Movies/Movies'));
+const MovieDetail = lazy(() => import('../page/MovieDetail/MovieDetail'));
 export const App = () => {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
+      {/* <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Movies />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetail />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
-    </div>
+    </Suspense>
   );
 };
